47439a2726f9ae116e105afed658d601bbfcf115
Mac <sathviknair@berkeley.edu>
2017-01-21 14:30:00 -0800

    Fixed scraper issues

--- scraper.py
+++ scraper.py
@@ -1,28 +1,33 @@
 import scrapy
 from scrapy.selector import Selector
 from scrapy.http import HtmlResponse
+from scrapy.crawler import CrawlerProcess
 from bs4 import BeautifulSoup
-import urllib.request import urlopen
+from urllib.request import urlopen
 
 class USDATableSpider(scrapy.Spider):
 	ingredients= {}
 	user_input = 'x' #variable assigned to input from user
 	name = 'USDA_table_spider'
 	start_urls = ['https://ndb.nal.usda.gov/ndb/search/list?ds=Standard%20Reference&qlookup=']
-	startrequests()
+	#startrequests()
 	#callback function will be a selector
 	#response = HtmlResponse(url=user_input, body=tbody) #url here depends on user input
 	#Selector(response=response).xpath('//span/text()').extract()
 	def parse(self, response):
-		url = start_urls[0] + user_input  # change to whatever your url is
-		page = urllib2.urlopen(url).read()
+		url = self.start_urls[0] + self.user_input  # change to whatever your url is
+		page = urlopen(url).read()
 		soup = BeautifulSoup(page)
 		for tr in soup.find_all('tr')[1:]:
     			tds = tr.find_all('td')
-			ingredients[tds[0].text]= tds[1].text
+    			self.ingredients[tds[0].text]= tds[1].text
+    			print(self.ingredients)
 		
-	parse(self, response)
-	
-	
+process = CrawlerProcess({
+    'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'
+})
+
+process.crawl(USDATableSpider)
+process.start()	
 	
 		
--- views.py
+++ views.py
@@ -1,4 +1,4 @@
-#from flask import Flask 
+from flask import Flask 
 import json
 from urllib.request import urlopen
 
