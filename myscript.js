9a9bc68375385e6e2235cd49b330f8ead25340fd
Ivon Liu <me@ivonliu.com>
2017-01-21 17:50:04 -0800

    Somewhat working hover

--- myscript.js
+++ myscript.js
@@ -1,14 +1,12 @@
 //var URL = 'http://localhost:3000/';
 var URL = 'http://107.170.77.233:3000/';
 
-//debugger;
 console.log('TEST TEST TEST');
-//$('.body-content').css('background-color', 'yellow');
 
-$('article.grid-col--fixed-tiles').css({"border-color": "red", "border-weight":"5px", "border-style":"solid"});
-
+// This will be an array of all the tiles that link to valid recipes
 var parsedItems = [];
 
+// Iterate through every tile and determine which link to valid recipes
 $('article.grid-col--fixed-tiles').each(function() {
     var gridItem = $(this);
     var id = "";
@@ -24,44 +22,37 @@ $('article.grid-col--fixed-tiles').each(function() {
     }
 });
 
+// Add hover thingy for each valid tile
 for (var i=0; i<parsedItems.length; i++) {
     var element = parsedItems[i].element;
     var id = parsedItems[i].recipeId;
     element.hover(
-        function() {
-            // mouse enter
-             
-            var xhr = new XMLHttpRequest();
-            xhr.open('GET', URL+'calculate/'+id, true);
-            xhr.onload = function() {
-                if (xhr.readyState == 4) {
-                    console.log(xhr.responseText);           
-                    element.before('<div id="'+id+'"><p>'+xhr.responseText+'</p></div>');
-                }
-            };
-            xhr.send();
-        }, function() {
-            // mouse exit
-            //$('#'+id).remove();
-            console.log('Mouse moved away from me now I am sad :(');
-        }
+        function(element, id) {
+            return function() {
+                // mouse enter
+                var xhr = new XMLHttpRequest();
+                xhr.open('GET', URL+'calculate/'+id, true);
+                xhr.onload = function() {
+                    if (xhr.readyState == 4) {
+                        console.log(xhr.responseText);
+                        $('.hover-info').remove();           
+                        element.before(
+                            // TODO Add styling
+                            '<div id="'+id+'" class="hover-info">' + 
+                                '<p>'+xhr.responseText+'</p>' +
+                            '</div>'
+                        );
+                    }
+                };
+                xhr.send();
+            }
+        }(element, id), 
+        function(element, id) {
+            return function() {
+                // mouse exit
+                $('#'+id).remove();
+                console.log('Mouse moved away from me now I am sad :(');
+            }
+        }(element, id)
     );
 }
-
-/*
-var processedItems = parsedItems.filter(function(item) {
-    
-    console.log('Sending requests for ' + item.recipeId);
-
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', 'http://localhost:3000/calculate/'+item.recipeId, true);
-    xhr.onload = function() {
-        if (xhr.readyState == 4) {
-            
-            console.log(xhr.responseText);
-        }
-    }
-    xhr.send();    
-});
-
-console.log('done');*/ 
