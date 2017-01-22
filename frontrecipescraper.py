81a5754d16b3a988a9fc5351e29e98ad171f65cc
Ivon Liu <me@ivonliu.com>
2017-01-21 23:36:58 -0800

    gkjk

--- frontrecipescraper.py
+++ frontrecipescraper.py
@@ -8,31 +8,40 @@ from urllib.request import urlopen
 current_id = 0 
 
 class RecipeSpider(scrapy.Spider):
-	ingredients= {}
-	#variable assigned to input from user
-	name = 'recipe_spider'
-	current_url = ['https://allrecipes.com/recipes/'+ str(current_id)]
-	final_dict= {}
-	#startrequests()
-	#callback function will be a selector
-	#response = HtmlResponse(url=user_input, body=tbody) #url here depends on user input
-	#Selector(response=response).xpath('//span/text()').extract()
+
+	def __init__(self, spider, current_id):
+		scrapy.Spider.__init__(self, spider)	
+
+		self.ingredients= {}
+		self.name = 'recipe_spider'
+		self.current_url = ['http://allrecipes.com/recipe/'+ str(current_id)]
+		self.final_dict= {}
+		#startrequests()
+		#callback function will be a selector
+		#response = HtmlResponse(url=user_input, body=tbody) #url here depends on user input
+		#Selector(response=response).xpath('//span/text()').extract()
+	
 	def parse(self, response):
 		list_of_ingred=[]
 		dict_of_recipes = {}
 		counter= 0
-		url= current_url
-		page= urlopen(url).read()
+		url = self.current_url[0]
+		print(url)
+		page = urlopen(url).read()
 		soup = BeautifulSoup(page)
-		for ul in soup.find_all('ul'): 
-			for li in soup.find_all('li', {'class': 'checklist__line'}): 
-				list_of_ingred += [li]
+		for li in soup.find_all('li', {'class': 'checkList__line'}):
+			inner_span = li.find('span') 
+			print(inner_span.text)
+			if inner_span.text and inner_span.text.lower() != 'add all ingredients to list':
+				list_of_ingred.append(inner_span.text) 
 		while list_of_ingred: 
 			dict_of_recipes[counter]= list_of_ingred[0]
 			counter += 1
 			list_of_ingred= list_of_ingred[1:]
+		print('sdfsafasdfsdfsaf', dict_of_recipes, type(dict_of_recipes))
 		return dict_of_recipes
 
-def call_this():
-	spidey = RecipeSpider(scrapy.Spider)
-	return spidey.parse(HtmlResponse(url=current_url))
+def call_this(current_id):	
+	spidey = RecipeSpider(scrapy.Spider, current_id)
+	print(spidey.current_url[0])
+	return spidey.parse(HtmlResponse(url=spidey.current_url[0]))
--- views.py
+++ views.py
@@ -2,36 +2,32 @@ from flask import Flask
 import json
 from urllib.request import urlopen
 import scraper
-
+import frontrecipescraper
 
 app = Flask(__name__)
 
-#food_id= scraper.call_this()
-
-#@app.route('/food/<food_id>', methods = ['GET', 'POST'])
 def find_info(food_id):
-	final_cal_dict= {}
-	count= 0
-	while count <= len(food_id):
-		url = 'http://api.nal.usda.gov/ndb/reports/?ndbno=' + str(food_id[count]) + '&type=f&format=json&api_key=3MNz36md1IPX44kwQVKy4s6z3OIC1akW4hRK3qlR'
-		print(url)
-		http = urlopen(url)
-		data = http.read().decode('utf-8')
-		food_data_json = json.loads(data)
-		nutrients = food_data_json['report']['food']['nutrients']
-		calories = [nutrient for nutrient in nutrients if nutrient['name'] == 'Energy' and nutrient['unit'] == 'kcal']
-		final_cal_dict[food_data_json['report']['food']['name']]= calories
-		count += 1
-	return final_cal_dict
+	url = 'http://api.nal.usda.gov/ndb/reports/?ndbno=' + str(food_id) + '&type=f&format=json&api_key=3MNz36md1IPX44kwQVKy4s6z3OIC1akW4hRK3qlR'
+	print(url)
+	http = urlopen(url)
+	data = http.read().decode('utf-8')
+	food_data_json = json.loads(data)
+	nutrients = food_data_json['report']['food']['nutrients']
+	calories = [nutrient for nutrient in nutrients if nutrient['name'] == 'Energy' and nutrient['unit'] == 'kcal']
+	return calories
 
 @app.route('/calories/<recipe_id>', methods=['GET'])
 def calculate_calories(recipe_id):
-	#ingredients = some_function()
-	ingredients = ['chicken']
-	for ingredient in ingredients:
+	ingredients = frontrecipescraper.call_this(recipe_id) 
+	print('\n\n\nasdf2', ingredients, type(ingredients), '\n\n\n')
+	for _, ingredient in ingredients.items():
+		print('asdf3', ingredient)
 		usda_code = scraper.call_this(ingredient)[0]
-		cal_dict = find_info('07935')
+		if usda_code == -1:
+			continue
+		cal_dict = find_info(usda_code)
 		print(cal_dict)
+	return recipe_id
 	
 
 if __name__ == "__main__":
