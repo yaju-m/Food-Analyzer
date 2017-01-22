7684cb8cb3e63e0468336ae8935d2f24f18fdf05
yaju-m <yaju-m@users.noreply.github.com>
2017-01-21 20:25:14 -0800

    Update views.py

--- views.py
+++ views.py
@@ -10,10 +10,15 @@ food_id= scraper.call_this()
 
 @app.route('/food/<food_id>', methods = ['GET', 'POST'])
 def find_info(food_id):
-	url = 'http://api.nal.usda.gov/ndb/reports/?ndbno=' + str(food_id[0]) + '&type=f&format=json&api_key=3MNz36md1IPX44kwQVKy4s6z3OIC1akW4hRK3qlR'
-	http = urlopen(url)
-	data = http.read().decode('utf-8')
-	food_data_json = json.loads(data)
-	nutrients = food_data_json['report']['food']['nutrients']
-	calories = [nutrient for nutrient in nutrients if nutrient['name'] == 'Energy' and nutrient['unit'] == 'kcal']
-	return calories
+	final_cal_dict= {}
+	count= 0
+	while count <= len(food_id):
+		url = 'http://api.nal.usda.gov/ndb/reports/?ndbno=' + str(food_id[count]) + '&type=f&format=json&api_key=3MNz36md1IPX44kwQVKy4s6z3OIC1akW4hRK3qlR'
+		http = urlopen(url)
+		data = http.read().decode('utf-8')
+		food_data_json = json.loads(data)
+		nutrients = food_data_json['report']['food']['nutrients']
+		calories = [nutrient for nutrient in nutrients if nutrient['name'] == 'Energy' and nutrient['unit'] == 'kcal']
+		final_cal_dict[food_data_json['report']['food']['name']= calories
+		count += 1
+	return final_cal_dict
