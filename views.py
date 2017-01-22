80f75bf49466be26962ed3811d0ecd63416d9861
yaju-m <yaju-m@users.noreply.github.com>
2017-01-21 17:41:53 -0800

    Update views.py

--- views.py
+++ views.py
@@ -1,11 +1,11 @@
-#from flask import Flask 
+from flask import Flask 
 import json
 from urllib.request import urlopen
 
 
-#app = Flask(__name__)
+app = Flask(__name__)
 
-#@app.route('/food/<food_id>', methods = ['GET', 'POST'])
+@app.route('/food/<food_id>', methods = ['GET', 'POST'])
 def find_info(food_id):
 	url = 'http://api.nal.usda.gov/ndb/reports/?ndbno=' + str(food_id[0]) + '&type=f&format=json&api_key=3MNz36md1IPX44kwQVKy4s6z3OIC1akW4hRK3qlR'
 	http = urlopen(url)
