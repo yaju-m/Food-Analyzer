bcad2d598190c2660fe12c274ee175778eda0cd0
yaju-m <yaju-m@users.noreply.github.com>
2017-01-21 19:23:22 -0800

    Update frontrecipescraper.py

--- frontrecipescraper.py
+++ frontrecipescraper.py
@@ -2,7 +2,7 @@ import scrapy
 from scrapy.selector import Selector
 from scrapy.http import HtmlResponse
 from scrapy.crawler import CrawlerProcess
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, SoupStrainer
 from urllib.request import urlopen
 from GoogleNLP import parse as text_parser
 from difflib import SequenceMatcher
@@ -15,28 +15,44 @@ def percent_similar(query, ingredients):
 	return ingredients[list_of_foods[correct_index]]
 
 
-class USDATableSpider(scrapy.Spider):
+class RecipeSpider(scrapy.Spider):
 	ingredients= {}
-	user_input = text_parser('6 pounds of bananas')[2] #variable assigned to input from user
-	name = 'USDA_table_spider'
-	start_urls = ['https://ndb.nal.usda.gov/ndb/search/list?ds=Standard%20Reference&qlookup=']
+	#variable assigned to input from user
+	name = 'recipe_spider'
+	start_urls = ['https://allrecipes.com/recipes/']
 	final_dict= {}
 	#startrequests()
 	#callback function will be a selector
 	#response = HtmlResponse(url=user_input, body=tbody) #url here depends on user input
 	#Selector(response=response).xpath('//span/text()').extract()
 	def parse(self, response):
-		url = self.start_urls[0] + self.user_input  # change to whatever your url is
-		page = urlopen(url).read()
+		list_of_recipes=[]
+		dict_of_recipes = {}
+		url= start_urls[0]
+		page= urlopen(url).read()
 		soup = BeautifulSoup(page)
-		for tr in soup.find_all('tr')[1:]:
-			tds = tr.find_all('td')
+		for h3 in soup.find_all('h3')[:]:
+			atags = h3.find_all('a')
+			list_of_recipes += [atags]
+			#need a list of hrefs in h3 in a. call is href_list
+		for link in BeautifulSoup(response, parseOnlyThese=SoupStrainer('a')):
+			if link.has_attr('href=/recipe/'):
+				while list_of_recipes and href_list:
+					dict_of_recipes[list_of_recipes[0]] = href_list[0]
+					list_of_recipes= list_of_recipes[1:]
+					href_list= href_list[1:]	
+			page = urlopen(url).read()
+			
+			
+			
 			number = str(tds[0].text)
 			food = str(tds[1].text)
 			number= number.replace('\t', '')
 			number= number.replace('\n', '')
 			food= food.replace('\t', '')
 			food= food.replace('\n', '')
+			
+			
 			self.ingredients[food]= number
 		self.final_dict[0]= percent_similar(self.user_input, self.ingredients)
 		return self.final_dict
