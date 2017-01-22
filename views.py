885e91b0063e87eb7da0b88c97d28d3cb7d26022
yaju-m <yaju-m@users.noreply.github.com>
2017-01-21 18:08:56 -0800

    Update views.py

--- views.py
+++ views.py
@@ -1,13 +1,15 @@
-from flask import Flask 
+#from flask import Flask 
 import json
 from urllib.request import urlopen
 import scraper
 
-app = Flask(__name__)
+
+
+#app = Flask(__name__)
 
 food_id= scraper.call_this()
 
-@app.route('/food/<food_id>', methods = ['GET', 'POST'])
+#@app.route('/food/<food_id>', methods = ['GET', 'POST'])
 def find_info(food_id):
 	url = 'http://api.nal.usda.gov/ndb/reports/?ndbno=' + str(food_id[0]) + '&type=f&format=json&api_key=3MNz36md1IPX44kwQVKy4s6z3OIC1akW4hRK3qlR'
 	http = urlopen(url)
